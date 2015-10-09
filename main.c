e2dda822d635383b67762bba17110998b6f79039
Jed <lejosnej@ainfosec.com>
2015-10-09 13:38:21 -0400

    Fix the sleeping logic
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- main.c
+++ main.c
@@ -8,21 +8,20 @@ int main()
   char buf[1024];
   unsigned int size;
   uint32_t index = 0;
-  int fast = 1000;
 
   xch = xc_interface_open(NULL, NULL, XC_OPENFLAG_NON_REENTRANT);
   while (1)
     {
       size = 1023;
       xc_readconsolering(xch, buf, &size, 0, 1, &index);
-      buf[size] = '\0';
-      printf(buf);
-      /* Be fast at the beginning, we don't want to stare at the
-	 screen for 2 minutes while all the old logs arrive */
-      /* if (fast > 0) */
-      /* 	fast--; */
-      /* else */
-      /* 	usleep(100000); */
+      if (size != 0) {
+	buf[size] = '\0';
+	printf(buf);
+      } else {
+	/* The call to xc_readconsolering() is non-blocking, so we
+	   sleep for 100ms when the last call didn't return anything */
+      	usleep(100000);
+      }
     }
 
   return 0;
